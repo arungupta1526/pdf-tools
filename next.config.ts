7ebeaf68323ce56728fe593499f882087fcc4e04
ArunGupta1526 <arungupta1526@gmail.com>
2026-03-05 19:47:55 +0530

    fix: take manual control of GH pages Next.js static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+// When running inside GitHub Actions, inject the repo name as basePath so all the chunk and asset requests succeed
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || 'pdf-tools';
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  output: "export"
+  output: "export",
+  basePath: basePath || undefined,
+  assetPrefix: assetPrefix || undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
